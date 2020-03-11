34f9dade04979c3a14bcade593d50bc5d111558f
elinsterz <42976247+emilylin-itp@users.noreply.github.com>
2020-03-11 16:49:54 -0400

    rfid p5js serial added

---
+++ website_code/making_code/making_coolingsystem_2/script/serial.js
@@ -0,0 +1,74 @@
+var serial;          // variable to hold an instance of the serialport library
+var portName = '/dev/tty.usbmodem14201';  // fill in your serial port name here
+var inData;                             // for incoming serial data
+var inString;
+
+function setup() {
+    serial = new p5.SerialPort(); // fill in your own IP address in place of the one shown here
+
+    //serial = new p5.SerialPort();       // make a new instance of the serialport library
+    serial.on('list', printList);  // set a callback function for the serialport list event
+    serial.on('connected', serverConnected); // callback for connecting to the server
+    serial.on('open', portOpen);        // callback for the port opening
+    serial.on('data', serialEvent);     // callback for when new data arrives
+    serial.on('error', serialError);    // callback for errors
+    serial.on('close', portClose);      // callback for the port closing
+
+    serial.list();                      // list the serial ports
+    serial.open(portName);              // open a serial port
+
+    createCanvas(400, 300);
+}
+
+// get the list of ports:
+function printList(portList) {
+    // portList is an array of serial port names
+    for (var i = 0; i < portList.length; i++) {
+        // Display the list the console:
+        console.log(i + " " + portList[i]);
+    }
+}
+
+function serverConnected() {
+    console.log('connected to server.');
+}
+
+function portOpen() {
+    console.log('the serial port opened.')
+}
+
+function serialEvent() {
+    //console.log('a');
+
+    inData = serial.readLine();
+  
+    console.log(inData);
+
+    //if tag A go to this url
+    if (inData == "Tag A") {
+        window.location.href = "http://www.google.com";
+    }
+
+    //if tag B go to this url
+    if (inData == "Tag B") {
+        window.location.href = "http://www.twitter.com";
+    }
+}
+
+function serialError(err) {
+    console.log('Something went wrong with the serial port. ' + err);
+}
+
+function portClose() {
+    console.log('The serial port closed.');
+}
+
+
+function draw() {
+    //   background(255, 255, 0);
+    //   fill(0);
+    //   text("Tag Name: " + inData, 30, 30);
+    //text("Tag Name: " + inString, 30, 30);
+    //console.log(inString);
+    // console.log(inData);
+}
