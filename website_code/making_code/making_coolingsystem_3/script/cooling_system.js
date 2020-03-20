f6e41f243dbe2a308b7ef2f82f689f2d6e851a55
emilylin-itp.github.io <el3015@nyu.edu>
2020-03-19 22:53:04 -0400

    cooling fan added

--- website_code/making_code/making_coolingsystem_3/script/cooling_system.js
+++ website_code/making_code/making_coolingsystem_3/script/cooling_system.js
@@ -9,33 +9,15 @@ function init() {
             newWindow();
         });
     }
-
-
-    // //change line height when window below certain width
-    // let headerText = document.getElementsByClassName('header-text');
-
-    // if (window.attachEvent) {
-    //     window.attachEvent('onresize', function () {
-    //         if (window.innerWidth < 760)
-    //             console.log("Less than 760");
-    //         else
-    //             console.log("More than 760");
-    //     });
-    // } else if (window.addEventListener) {
-    //     window.addEventListener('resize', function () {
-    //         if (window.innerWidth < 760) {
-    //             // console.log("Less than 760");
-    //             headerText.style.lineHeight = '0.8em';
-    //         } else {
-    //             //console.log("More than 760");
-    //             headerText.style.lineHeight = '0em';
-    //         }
-    //     });
-    // }
 }
 
-function newWindow(){
+function openHeatPipe(){
     window.location.pathname = 'heat_pipe.html'
+}
+
+
+function openCoolingFan(){
+    window.location.pathname = 'cooling_fan.html'
 }
 
 ///////* CHANGE COLOR OF LETTER WHEN BUTTON IS CLICKED */////////
