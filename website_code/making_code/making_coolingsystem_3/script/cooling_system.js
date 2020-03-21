da357d64c162a813d3d86a753b57ddc5c9b35e5c
emilylin-itp.github.io <el3015@nyu.edu>
2020-03-21 11:34:19 -0400

    all hyperlinked!

--- website_code/making_code/making_coolingsystem_3/script/cooling_system.js
+++ website_code/making_code/making_coolingsystem_3/script/cooling_system.js
@@ -1,23 +1,30 @@
 function init() {
 
-    //click green arrow
-    let leftArrow = document.getElementsByClassName('left-arrow');
+    // //click green arrow
+    // let leftArrow = document.getElementsByClassName('left-arrow');
 
-    for (var i = 0; i < leftArrow.length; i++) {
-        leftArrow[i].addEventListener('click', () => {
-            greenArrow();
-            newWindow();
-        });
-    }
+    // for (var i = 0; i < leftArrow.length; i++) {
+    //     leftArrow[i].addEventListener('click', () => {
+    //         greenArrow();
+    //         newWindow();
+    //     });
+    // }
+}
+
+function openHeatSink(){
+    window.location.pathname = 'heat_sink.html'
 }
 
 function openHeatPipe(){
     window.location.pathname = 'heat_pipe.html'
 }
 
-
 function openCoolingFan(){
     window.location.pathname = 'cooling_fan.html'
+}
+
+function openElectricalWires(){
+    window.location.pathname = 'electrical_wire.html'
 }
 
 ///////* CHANGE COLOR OF LETTER WHEN BUTTON IS CLICKED */////////
